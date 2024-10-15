f755166567800e94ced7c6b81544f4faaf8e45cf
bin16 <11701531+bin16@users.noreply.github.com>
2024-10-15 13:27:12 +0800

    save util to read and write files, across os and wasm

--- game/save/save.go
+++ game/save/save.go
@@ -5,32 +5,60 @@ import (
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
-var DIR = "."
+var (
+	wd = "."
 
-var Read = osRead
-var Write = osWrite
+	ReadSave  = osReadJSON
+	WriteSave = osWriteJSON
+
+	ReadJSON = osReadJSON
+	ReadFile = osReadFile
+
+	List = osList
+	Find = osFind
+)
 
 func init() {
 	if d, err := os.Getwd(); err != nil {
 		log.Println(err)
-		DIR = ""
+		wd = ""
 	} else {
-		DIR = d
+		wd = d
 	}
 }
 
-// func Read(name string, data any) error {
-// 	return osRead(name, data)
-// }
+// List entries of dir,
+// {name} relative to {wd}
+func osList(name string) ([]string, error) {
+	var names = []string{}
 
-// func Write(name string, data any) error {
-// 	return osWrite(name, data)
-// }
+	var pathname = path.Join(wd, name)
+	entries, err := os.ReadDir(pathname)
+	if err != nil {
+		return names, err
+	}
 
-func osRead(name string, data any) error {
-	var filename = path.Join(DIR, name)
+	for _, d := range entries {
+		names = append(names, d.Name())
+	}
+
+	return names, err
+}
+
+// Read file and return bytes,
+// {name} relative to {wd}
+func osReadFile(name string) ([]byte, error) {
+	var filename = path.Join(wd, name)
+	return os.ReadFile(filename)
+}
+
+// Read data as JSON,
+// {name} relative to {wd}
+func osReadJSON(name string, data any) error {
+	var filename = path.Join(wd, name)
 	raw, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -39,8 +67,36 @@ func osRead(name string, data any) error {
 	return json.Unmarshal(raw, data)
 }
 
-func osWrite(name string, data any) error {
-	var filename = path.Join(DIR, name)
+// Find entries has {suffix}, return list of string
+// {name} relative to {wd}
+func osFind(dir, suffix string) ([]string, error) {
+	var (
+		results = []string{}
+		p       = path.Join(wd, dir)
+	)
+
+	entries, err := os.ReadDir(p)
+	if err != nil {
+		return results, err
+	}
+
+	for _, d := range entries {
+		if d.IsDir() {
+			continue
+		}
+
+		if strings.HasSuffix(d.Name(), suffix) {
+			results = append(results, d.Name())
+		}
+	}
+
+	return results, nil
+}
+
+// Write JSON data to file,
+// {name} relative to {wd}
+func osWriteJSON(name string, data any) error {
+	var filename = path.Join(wd, name)
 
 	raw, err := json.Marshal(data)
 	if err != nil {
--- game/save/save_js.go
+++ game/save/save_js.go
@@ -2,15 +2,25 @@ package save
 
 import (
 	"encoding/json"
+	"io"
+	"net/http"
+	"path"
+	"regexp"
 	"syscall/js"
 )
 
 func init() {
-	Read = jsRead
-	Write = jsWrite
+	ReadSave = localStorageReadJSON
+	WriteSave = localStorageWriteJSON
+
+	ReadJSON = httpReadJSON
+	ReadFile = httpReadBytes
+
+	List = httpList
+	Find = httpFind
 }
 
-func jsWrite(name string, data any) error {
+func localStorageWriteJSON(name string, data any) error {
 	raw, err := json.Marshal(data)
 	if err != nil {
 		return err
@@ -20,9 +30,82 @@ func jsWrite(name string, data any) error {
 	return nil
 }
 
-func jsRead(name string, data any) error {
+func localStorageReadJSON(name string, data any) error {
 	var val = js.Global().Get("localStorage").Call("getItem", name)
 	var str = val.String()
 
 	return json.Unmarshal([]byte(str), data)
+}
+
+func httpReadJSON(name string, data any) error {
+	var u = path.Join("/", name)
+
+	resp, err := http.Get(u)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(data)
+}
+
+func httpList(dir string) ([]string, error) {
+	var names = []string{}
+
+	resp, err := http.Get(path.Join("/", dir))
+	if err != nil {
+		return names, err
+	}
+	defer resp.Body.Close()
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return names, err
+	}
+
+	var rx = regexp.MustCompile(`href="([^\"]+)"`)
+	var results = rx.FindAll(raw, -1)
+	for _, s := range results {
+		names = append(names, string(s[6:len(s)-1]))
+	}
+
+	return names, nil
+}
+
+func httpFind(dir, suffix string) ([]string, error) {
+	var names = []string{}
+
+	resp, err := http.Get(path.Join("/", dir))
+	if err != nil {
+		return names, err
+	}
+	defer resp.Body.Close()
+
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return names, err
+	}
+
+	var rx = regexp.MustCompile(`"\w+` + suffix + `"`)
+	var results = rx.FindAll(raw, -1)
+	for _, s := range results {
+		s = s[1 : len(s)-1]
+		names = append(names, string(s))
+	}
+
+	// fmt.Println(string(raw), names)
+
+	return names, nil
+}
+
+func httpReadBytes(name string) ([]byte, error) {
+	var u = path.Join("/", name)
+
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
 }
